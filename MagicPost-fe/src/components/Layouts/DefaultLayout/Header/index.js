8a25e79e0a5bf47c769b323194479a33f381c7e2
21020383-NGUYENMINHQUAN <21020383@vnu.edu.vn>
2023-12-11 20:57:47 +0700

    .

--- MagicPost-fe/src/components/Layouts/DefaultLayout/Header/index.js
+++ MagicPost-fe/src/components/Layouts/DefaultLayout/Header/index.js
@@ -1,13 +1,15 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import logo from 'src/res/MagicPost-logos.jpeg';
 
 const cx = classNames.bind(styles);
 function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('content')}>
-                {/* Logo */}
-                {/* Search */}
+                <div className={cx('logo')}>
+                    <img src={logo} alt="MagicPost" />
+                </div>
             </div>
         </header>
     );
