cd55d9d5a53f713c8b1c7a4ac27e013b274d1a6e
21020383-NGUYENMINHQUAN <21020383@vnu.edu.vn>
2023-12-13 23:03:27 +0700

    update search bar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,21 +5,22 @@ const cx = classNames.bind(styles);
 
 function Home() {
     return (
-        <div className={cx('search-bar')}>
-            <span className={cx('wrapper')}>
-                <h1>Theo dõi đơn hàng</h1>
-                <input
-                    placeholder="Nhập mã đơn hàng bạn cần tra cứu"
-                    className={cx('input')}
-                    type="text"
-                    spellCheck={false}
-                />
-
-                <span className={cx('btn')}>
-                    <button type="button" className={cx('search-btn')}>
-                        <span>Theo dõi</span>
-                    </button>
-                </span>
+        <div className={cx('wrapper')}>
+            <h1>Theo dõi đơn hàng</h1>
+            <span className={cx('search-box')}>
+                <div className={cx('search-bar')}>
+                    <input
+                        placeholder="Nhập mã đơn hàng bạn cần tra cứu"
+                        className={cx('input')}
+                        type="text"
+                        spellCheck={false}
+                    />
+                    <span className={cx('btn')}>
+                        <button type="button" className={cx('search-btn')}>
+                            <span>Theo dõi</span>
+                        </button>
+                    </span>
+                </div>
             </span>
         </div>
     );
