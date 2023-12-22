4f059a9d796e4ae111dfac9f97dbbb73eb501272
ppvan <phuclaplace@gmail.com>
2023-12-22 22:10:02 +0700

    feat: code the navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
-import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
+import { MainNav } from '@/components/main-nav'
+import { Brand } from '@/components/brand'
 
 export default function Home() {
     return (
         <>
-            <h1>Hello World</h1>
+            <div className="hidden flex-col md:flex">
+                <div className="border-b">
+                    <div className="flex h-16 items-center px-4">
+                        <Brand title="Magic Post" />
+                        <MainNav className="mx-6" />
+                        <div className="ml-auto flex items-center space-x-4">
+                            {/* <Search /> */}
+                            {/* <UserNav /> */}
+                        </div>
+                    </div>
+                </div>
+                <div className="flex-1 space-y-4 p-8 pt-6">
+                    <div className="flex items-center justify-between space-y-2">
+                        <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+                        <div className="flex items-center space-x-2">
+                            {/* <CalendarDateRangePicker /> */}
+                            <Button>Download</Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
