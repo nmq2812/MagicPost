28c7298b9caaab6ca0656d8dc86fd55a8be59af5
ppvan <phuclaplace@gmail.com>
2023-12-24 10:19:24 +0700

    feat: change minor thing

--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,10 @@ export default function Home() {
         <>
             <div className="hidden flex-col md:flex sm:grid">
                 <div className="border-b">
-                    <div className="flex h-16 items-center px-4 fixed top-0 w-full z-50">
+                    <div className="flex h-16 items-center px-4 fixed border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 top-0 w-full z-50">
                         <Brand title="Magic Post" />
                         <MainNav className="mx-6" />
                         <div className="ml-auto flex items-center space-x-4">
-                            {/* <Search /> */}
-                            {/* <UserNav /> */}
                         </div>
                     </div>
                 </div>
@@ -25,13 +23,6 @@ export default function Home() {
                     <Intro />
                     <Services />
                     <Search />
-                    <div className="flex items-center justify-between space-y-2">
-                        <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-                        <div className="flex items-center space-x-2">
-                            {/* <CalendarDateRangePicker /> */}
-                            <Button>Download</Button>
-                        </div>
-                    </div>
                 </div>
 
                 <Footer></Footer>
