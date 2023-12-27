3949ed9092e830c1f829fc0f02946ff61ea7a108
ppvan <phuclaplace@gmail.com>
2023-12-27 18:27:29 +0700

    feat: update avatar UI

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,7 +2,7 @@
 import { Brand } from "@/components/brand"
 import Link from "next/link"
 import { cn } from "@/lib/utils"
-import { LogOut, PieChart, Settings, Store, Users, Warehouse } from "lucide-react"
+import { LogOut, PieChart, Settings, Store, User, Users, Warehouse } from "lucide-react"
 import { SidebarNav } from "@/components/admin-nav"
 
 interface AdminLayoutProps {
@@ -44,14 +44,16 @@ export default function AdminLayout({ children }: AdminLayoutProps) {
         <div className="flex gap-2 h-screen w-full p-1 rounded-lg">
             <aside className="w-1/6 border rounded-lg bg-background shadow">
                 <div className="flex flex-col px-2 py-3 h-full">
-                    <div className="p-2 mb-4 bg-card">
+                    <div className="p-2 mb-2 bg-card">
                         <Brand title="Magic Post" />
                     </div>
 
-                    <div>
-                        <img className="object-cover w-24 h-24 mx-auto mb-3 rounded-full" src="https://avatars.githubusercontent.com/ppvan" alt="@ppvan" />
-                        <p className="text-center font-large leading-none">Phạm Văn Phúc</p>
-                        <p className="text-center text-muted-foreground">Admin</p>
+                    <div className="flex items-center justify-start gap-2 p-4 mb-4">
+                        <User />
+                        <div className="flex flex-col items-center">
+                            <p className="font-large">@ppvan</p>
+                            <p className="text-muted-foreground">Admin</p>
+                        </div>
                     </div>
 
                     <SidebarNav items={navigationLinks} />
